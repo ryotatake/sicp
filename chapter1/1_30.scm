35c8127600cc295066730569f93d95f928fcbe45
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2021-03-03 21:24:25 +0900

    solve 1.30

---
+++ chapter1/1_30.scm
@@ -0,0 +1,14 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (sum-integers a b)
+  (define (identity x) x)
+  (define (inc x) (+ x 1))
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+; 55
