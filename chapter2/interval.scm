e0045d715887045d0c8993a46695cf64b6143b69
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2021-03-10 20:48:16 +0900

    solve 2.7

---
+++ chapter2/interval.scm
@@ -0,0 +1,31 @@
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound x) (max (car x) (cdr x)))
+(define (lower-bound x) (min (car x) (cdr x)))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+; xとyの小さい方同士でかけたものが一番小さくならないのは、どちらもマイナスの可能性があるため。
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+    x
+    (make-interval (/ 1.0 (upper-bound y))
+                   (/ 1.0 (lower-bound y)))))
+
+(define (display-interval x)
+  (display (car x))
+  (display ",")
+  (display (cdr x)))
+
+(define x (make-interval 1 6))
+(define y (make-interval 2 3))
+(display-interval (add-interval x y))
